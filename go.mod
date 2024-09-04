9a396e1e3e1857121851ccf6352c46e1d833e020
kumarmo2 <kr.mohit6794@gmail.com>
2024-09-04 18:56:44 +0530

    done unqiue id

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/kumarmo2/maelstrom-challenge-go
 
 go 1.23.0
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240813160128-8b9e94c75e59 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240813160128-8b9e94c75e59 // indirect
+	github.com/oklog/ulid/v2 v2.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"encoding/json"
 	"log"
+	// "math/rand"
+	//
+	// "time"
 
+	"github.com/google/uuid"
 	"github.com/jepsen-io/maelstrom/demo/go"
+	// crate "github.com/kumarmo2/maelstrom-challenge-go/maelstromchallengego"
+	// ulid "github.com/oklog/ulid/v2"
 )
 
 func main() {
@@ -12,6 +18,7 @@ func main() {
 	node := maelstrom.NewNode()
 
 	node.Handle("echo", handlerGenerator(node, handleEcho))
+	node.Handle("generate", handlerGenerator(node, handleGenerate))
 
 	err := node.Run()
 	if err != nil {
@@ -20,8 +27,30 @@ func main() {
 
 }
 
-func handlerGenerator(node *maelstrom.Node, h func(node *maelstrom.Node) maelstrom.HandlerFunc) func(msg maelstrom.Message) error {
+func handlerGenerator(node *maelstrom.Node, h func(node *maelstrom.Node) maelstrom.HandlerFunc) maelstrom.HandlerFunc {
 	return h(node)
+}
+
+func handleGenerate(node *maelstrom.Node) maelstrom.HandlerFunc {
+	return func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		err := json.Unmarshal(msg.Body, &body)
+		if err != nil {
+			return err
+		}
+		// var entropy = crate.NewRand()
+		// entropy := rand.New(rand.NewSource(time.Now().UnixNano()))
+		// id, err := ulid.New(ulid.Timestamp(time.Now()), entropy)
+		// if err != nil {
+		// 	return err
+		// }
+		id := uuid.New()
+
+		body["type"] = "generate_ok"
+		body["id"] = id
+		return node.Reply(msg, body)
+	}
 }
 
 func handleEcho(node *maelstrom.Node) maelstrom.HandlerFunc {
