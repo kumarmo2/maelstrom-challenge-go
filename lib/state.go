a957e794ca68f0df14fa4ded7e8896830f812ca1
kumarmo2 <kr.mohit6794@gmail.com>
2024-10-01 12:04:24 +0530

    minor changes

--- lib/state.go
+++ lib/state.go
@@ -23,41 +23,63 @@ func newMessageItem(num int) *MessageItem {
 }
 
 func (message *MessageItem) Key() int {
-	return message.num
+	return int(message.time.UnixMilli())
+}
+
+type MessageStore struct {
+	MessageMap map[int]bool
+	messages   *AVLTree[*MessageItem]
+}
+
+func (self *MessageStore) InsertItem(message int) {
+	msg := newMessageItem(message)
+	self.MessageMap[message] = true
+	self.messages.InsertItem(msg)
+}
+
+func (self *MessageStore) ContainsKey(message int) bool {
+	_, exists := self.MessageMap[message]
+	return exists
 }
 
 type NodeState struct {
-	msgLock    *sync.RWMutex
-	messages   *AVLTree[*MessageItem]
-	nodeId     string
-	otherNodes map[string]*NodeMetaInfo
+	msgLock *sync.RWMutex
+	// messages           *AVLTree[*MessageItem]
+	messageStore       *MessageStore
+	nodeId             string
+	otherNodesMetaInfo map[string]*NodeMetaInfo
+	otherNodes         []string
 }
 
 func NewNodeState(node *maelstrom.Node) *NodeState {
-	self := &NodeState{msgLock: &sync.RWMutex{}, messages: NewAVLTRee[*MessageItem]()}
+	store := &MessageStore{MessageMap: make(map[int]bool), messages: NewAVLTRee[*MessageItem]()}
+	self := &NodeState{msgLock: &sync.RWMutex{}, messageStore: store}
 	self.nodeId = node.ID()
 	allNodes := node.NodeIDs()
 
-	self.otherNodes = make(map[string]*NodeMetaInfo)
+	self.otherNodesMetaInfo = make(map[string]*NodeMetaInfo)
+	self.otherNodes = make([]string, 0)
 
 	for _, n := range allNodes {
 		if n != self.nodeId {
-			self.otherNodes[n] = &NodeMetaInfo{name: n, lastSync: time.Now().Add(time.Minute)}
+			self.otherNodesMetaInfo[n] = &NodeMetaInfo{name: n, lastSync: time.Now().Add(time.Minute)}
+			self.otherNodes = append(self.otherNodes, n)
 		}
 	}
+	// go self.BackgroundSync()
 	return self
 }
 
 func (self *NodeState) InsertMessage(message int) {
 	self.msgLock.Lock()
 	defer self.msgLock.Unlock()
-	msg := newMessageItem(message)
-	self.messages.InsertItem(msg)
+	// msg := newMessageItem(message)
+	self.messageStore.InsertItem(message)
 }
 
 func (self *NodeState) SaveBroadcastMessageIfNew(message int, node *maelstrom.Node) error {
 	self.msgLock.RLock()
-	exists := self.messages.ContainsKey(message)
+	exists := self.messageStore.ContainsKey(message)
 	if exists {
 		// No need to broadcast further.
 		self.msgLock.RUnlock()
@@ -70,7 +92,7 @@ func (self *NodeState) SaveBroadcastMessageIfNew(message int, node *maelstrom.No
 	var body map[string]any = map[string]any{}
 	body["type"] = "broadcast"
 	body["message"] = message
-	for _, nbt := range self.otherNodes {
+	for _, nbt := range self.otherNodesMetaInfo {
 		err := node.Send(nbt.name, body)
 		if err != nil {
 			return err
@@ -79,17 +101,26 @@ func (self *NodeState) SaveBroadcastMessageIfNew(message int, node *maelstrom.No
 	return nil
 }
 
-func (self *NodeState) BackgroundSync() {
-	for {
-		time.Now()
-		time.Sleep(700)
-		if self.nodeId == "" {
-			continue
-		}
-		// _ := getRandomNodes(self.otherNodes)
-
-	}
-}
+// func (self *NodeState) BackgroundSync() {
+// 	for {
+// 		time.Now()
+// 		time.Sleep(1000)
+// 		// if self.nodeId == "" {
+// 		// 	continue
+// 		// }
+// 		nodesToSync := getRandomNodes(self.otherNodes)
+// 		// log.Printf("nodesToSync: %v\n", nodesToSync)
+//
+// 		for _, nodeToSync := range nodesToSync {
+// 			nodeMeta, exists := self.otherNodesMetaInfo[nodeToSync]
+// 			if !exists {
+// 				continue
+// 			}
+//
+// 		}
+//
+// 	}
+// }
 
 func getRandomNodes(otherNodes []string) []string {
 	randNodes := make([]string, 0, 2)
@@ -114,9 +145,9 @@ func getRandomNodes(otherNodes []string) []string {
 }
 
 // func (self *NodeState) ReadMessages(callback func(messages map[int]bool)) {
-func (self *NodeState) ReadMessages(callback func(messages *AVLTree[*MessageItem])) {
+func (self *NodeState) ReadMessages(callback func(messages *MessageStore)) {
 	self.msgLock.RLock()
 	defer self.msgLock.RUnlock()
-	callback(self.messages)
+	callback(self.messageStore)
 
 }
